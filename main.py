71075ea792889910c8368cf1a392e4472123545a
Kontur <sequence@myrealm.ca>
2021-09-01 17:27:43 +0200

    Removed an unnecessary construction in a function.

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def get_homeworks_status_updates(telegram_bot, telegram_chat_id, headers,
                                  no_connection_count, timestamp=None):
     api_url = "https://dvmn.org/api/long_polling/"
     try:
-        params = {"timestamp": timestamp} if timestamp else None
+        params = {"timestamp": timestamp}
         response = requests.get(api_url, headers=headers, params=params)
         response.raise_for_status()
         no_connection_count = 0
